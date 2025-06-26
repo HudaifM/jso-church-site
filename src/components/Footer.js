e4badff4472c377bd7aea0f619cfd739faa33745
HudaifM <hudaifath.m@mountbox.in>
2025-06-26 12:05:53 +0530

    Footer Css created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './Footer.css'; // Importing the CSS file for styling
 
 const Footer = () => (
   <footer className="footer">
-    <p>&copy; {new Date().getFullYear()} My React Website</p>
+    <p>&copy; {new Date().getFullYear()} JSO Church. All rights reserved.</p>
   </footer>
 );
 
